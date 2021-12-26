f37332f589ebffa5d00e4cd177e5276526015a90
iriskron <iris.kronfeld@gmail.com>
2021-12-26 23:14:38 +0200

    implemented server functionality, can only run server one time for now, fix will come later after some more testing1

--- server.py
+++ server.py
@@ -9,65 +9,69 @@ class Server:
         self.ip_address = get_if_addr('eth1')
         self.player_names = ['', '']
         self.player_count = 0
-        self.tcp_socket = None
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.tcp_socket.bind((self.ip_address, 4567))
         self.client_answer = [-1, '']
 
     def send_udp_offers(self):
-        msg = bytes.fromhex('abcddcba02' + str(8888))
+        msg = bytes.fromhex('abcddcba02' + str(4567))
+        print(f'Server started, listening on IP address {self.ip_address}')
         while self.player_count < 2:
-            print(f'Server started, listening on IP address {self.ip_address}')
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            sock.bind((self.ip_address, 3332))
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-            sock.bind((self.ip_address, 8888))
             sock.sendto(msg, ("255.255.255.255", 13117))
             sock.close()
             sleep(1)
 
-    def tcp_client_connect(self):
-        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.tcp_socket.listen(2)
-        clients = []
+    def tcp_client_connect(self, clients):
+        self.tcp_socket.listen()
         while self.player_count < 2:
             try:
                 client_socket, (client_ip, client_port) = self.tcp_socket.accept()
                 clients.append([client_socket, (client_ip, client_port)])
-                player_name = client_socket.recv(1024)
+                player_name = str(client_socket.recv(1024), 'utf8')
                 client_socket.settimeout(10)
                 self.player_names[self.player_count] = str(player_name)
                 self.player_count += 1
             except Exception as e:
                 print(f'Unable to connect to client.\n Exception received: {str(e)}')
-        return clients
 
     def play_game(self, clients):
         ans, eq = self.question_bank()
-        self.tcp_socket.send(f'Welcome to Quick Maths.'
-                             f'\nPlayer 1: {self.player_names[0]}'
-                             f'\nPlayer 2: {self.player_names[1]}'
-                             f'\n=='
-                             f'\nPlease answer the following question as fast as you can:\n'
-                             f'How much is {eq}?')
+        msg = f'Welcome to Quick Maths.' \
+              f'\nPlayer 1: {self.player_names[0]}' \
+              f'\nPlayer 2: {self.player_names[1]}' \
+              f'\n==' \
+              f'\nPlease answer the following question as fast as you can:\n' \
+              f'How much is {eq}?'
+        clients[0][0].sendall(bytes(msg, 'utf8'))
+        clients[1][0].sendall(bytes(msg, 'utf8'))
 
-        t1 = threading.Thread(target=self.get_answer, args=(clients[0], self.player_names[0]))
-        t2 = threading.Thread(target=self.get_answer, args=(clients[1], self.player_names[1]))
+        t1 = threading.Thread(target=self.get_answer, args=(clients[0][0], self.player_names[0]))
+        t2 = threading.Thread(target=self.get_answer, args=(clients[1][0], self.player_names[1]))
         t1.start()
         t2.start()
         t1.join()
         t2.join()
 
         if self.client_answer == [-1, '']:
-            self.tcp_socket.send(f"Game over!"
-                                 f"\nThe correct answer was {ans}!"
-                                 f"\nNo Winners - Ran out of time!")
+            msg = f"Game over!" \
+                  f"\nThe correct answer was {ans}!" \
+                  f"\nNo Winners - Ran out of time!"
         elif self.client_answer[0] == ans:
-            self.tcp_socket.send(f"Game over!"
-                                 f"\nThe correct answer was {ans}!"
-                                 f"\nCongratulations to the winner: {self.client_answer[1]}")
+            msg = f"Game over!" \
+                  f"\nThe correct answer was {ans}!" \
+                  f"\nCongratulations to the winner: {self.client_answer[1]}"
         else:
-            self.tcp_socket.send(f"Game over!"
-                                 f"\nThe correct answer was {ans}!"
-                                 f"\nCongratulations to the winner: {self.player_names[0] if self.player_names[1] == self.client_answer[1] else self.player_names[1]}")
+            msg = f"Game over!" \
+                  f"\nThe correct answer was {ans}!" \
+                  f"\nCongratulations to the winner: {self.player_names[0] if self.player_names[1] == self.client_answer[1] else self.player_names[1]}"
+
+        clients[0][0].send(bytes(msg, 'utf8'))
+        clients[1][0].send(bytes(msg, 'utf8'))
 
         self.client_answer = [-1, '']
         self.player_names = ['', '']
@@ -85,9 +89,6 @@ class Server:
             self.client_answer[0] = ans
             self.client_answer[1] = team_name
         mutex.release()
-
-    def run_server(self):
-        pass
 
     def question_bank(self):
         operator_functions = {
@@ -117,7 +118,18 @@ class Server:
             except ZeroDivisionError:
                 continue
         return ans, eq
+
+    def run_server(self):
+        clients = []
+        t1 = threading.Thread(target=self.send_udp_offers)
+        t2 = threading.Thread(target=self.tcp_client_connect, args=(clients,))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        self.play_game(clients)
+
+
 if __name__ == '__main__':
     server = Server()
-    #server.run_server()
-    server.question_bank()
+    server.run_server()
