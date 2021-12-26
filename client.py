a34b1715353207e0f3815a8ec7002dd3dd7fd17a
iriskron <iris.kronfeld@gmail.com>
2021-12-26 23:14:01 +0200

    minor client fixes

--- client.py
+++ client.py
@@ -8,34 +8,37 @@ class Client:
         self.server_port = None
         self.tcp_socket = None
 
-        self.TEAM_NAME = "Bruh"
+        self.TEAM_NAME = "Big Shaq"
         self.MAGIC_COOKIE = "abcddcba"
         self.MESSAGE_TYPE = "02"
 
     def look_for_server(self):
         print("Client started, listening for offer requests...")
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) # USE THIS ON LINUX
         sock.bind(('', 13117))
 
         while True:
             data, addr = sock.recvfrom(1024)
-            self.server_ip = addr[0]
             data_hex = data.hex()
             if data_hex[:8] != self.MAGIC_COOKIE:
-                print("Failed to connect to server: UDP packet didn't contain 0xabcddcba in MAGIC COOKIE field.")
+                # print("Failed to connect to server: UDP packet didn't contain 0xabcddcba in MAGIC COOKIE field.")
                 continue
             if data_hex[8:10] != self.MESSAGE_TYPE:
-                print("Failed to connect to server: UDP packet didn't contain 0x02 in MESSAGE TYPE field.")
+                # print("Failed to connect to server: UDP packet didn't contain 0x02 in MESSAGE TYPE field.")
                 continue
+            self.server_ip = addr[0]
+            print(self.server_ip)
             self.server_port = int(data_hex[10:])
+            print(self.server_ip, self.server_port)
             sock.close()
             break
 
     def connect_to_server(self):
         print(f'Received offer from {self.server_ip}, attempting to connect...')
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         self.tcp_socket.settimeout(3)
 
         try:
@@ -45,17 +48,18 @@ class Client:
             return False
 
         print(f'Successfully connected to server {self.server_ip}')
-        team_name_as_bytes = bytes(self.TEAM_NAME + '\n')
-        self.tcp_socket.send(team_name_as_bytes)
+        team_name_as_bytes = bytes(self.TEAM_NAME + '\n', 'utf8')
+        self.tcp_socket.sendall(team_name_as_bytes)
+        self.tcp_socket.settimeout(None)
         return True
 
     def get_msg_from_server(self):
-        server_msg = str(self.tcp_socket.recv(1024))
+        server_msg = str(self.tcp_socket.recv(1024), 'utf8')
         print(server_msg)
 
     def send_client_answer(self):
         ans = input('Please enter the answer to the math problem: ')[0]
-        self.tcp_socket.send(bytes(ans))
+        self.tcp_socket.send(bytes(ans, 'utf8'))
 
     def run_client(self):
         while True:
